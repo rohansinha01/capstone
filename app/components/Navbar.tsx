6ba3411303cd985ef1981b0db06e653aeadc80fa
rohansinha01 <rohansinha01@gmail.com>
2024-04-13 19:41:44 -0400

    hamburger menu

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,74 @@
+'use client'
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+export const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
+  function getMenuClasses() {
+    let menuClasses = []
+            if (isOpen) {
+                menuClasses = [
+                   "flex", 
+                   "absolute",
+                   "top-[60px]",
+                   "bg-gray-800",
+                   "w-full",
+                   "p-4",
+                   "left-0",
+                   "gap-10",
+                   "flex-col"
+                ]
+            } else {
+                menuClasses.push("hidden");
+            }
+
+            return menuClasses.join("")
+        }
+    return (
+        <nav className="bg-gray-800 text-white p-4 sm:p-6 md:flex md: justify-between md: items-center">
+           <div className="container mx-auto flex justify-between items-center">  
+            <Link href="/" className="container mx-auto flex justify-between items-center">Did I Like That</Link>
+           
+                
+                <div className={getMenuClasses()}>
+                    <Link  href="/new" className="mx-2 hover: text-gray-300">New Entry</Link>
+                    <Link  href="https://github.com/rohansinha01/capstone" className="mx-2 hover: text-gray-300">Github</Link>
+                    <Link  href="/faq" className="mx-2 hover: text-gray-300">FAQ</Link>
+                </div>
+
+                <div>
+                    <button onClick={() => {
+                        setIsOpen(!isOpen)
+                    }}>
+                         <svg className="w-6 h-6"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg">
+                            
+                            {isOpen ? (
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M6 18L18 6M6 6l12 12"
+                            ></path>
+                           ) : (
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M4 6h16M4 12h16m-7 6h7"
+                            ></path>
+                            )}
+                         </svg>
+                       
+                    </button>
+                </div>
+            </div>
+            
+
+        </nav>
+    )
+}
