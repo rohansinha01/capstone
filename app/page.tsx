e239669ec696de6a2378393fa46351c6e5e4b4c2
rohansinha01 <rohansinha01@gmail.com>
2024-04-17 22:05:17 -0400

    frontend index and delete

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,84 @@
+"use client"
 
+import { useEffect, useState } from "react";
 import { Navbar } from "./components/Navbar";
 import Link from "next/link";
 
+interface MMedia {
+  $id: string;
+  name: string;
+}
+
 
 export default function Home() {
+  const [media, setMedia] = useState<MMedia[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null)
+
+  useEffect(() => {
+    const fetchMedia = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch('/api/media');
+        if(!response.ok) {
+          throw new Error("Failed to fetch media")
+        }
+            const data = await response.json();
+            setMedia(data)
+      } catch (error) {
+        console.log("Error: ", error);
+        setError("Failed to load media. Please try reloading the page")
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchMedia()
+    }, [])
+
+  const handleDelete = async (id: string) => {
+    try {
+      await fetch(`/api/media/${id}`, { method: "DELETE"});
+      setMedia((prevMedia) =>
+      prevMedia?.filter((i) => i.$id !== id)
+      )
+    } catch (error) {
+      setError("Failed to delete media. Please try again.")
+    }
+  }
+
   return (
     <>
     <Navbar />
-    <div className="mx-16">
-    <div className="p-4 my-2 rounded-md border-b leading-8">
-      <div className="font-bold">Media - <span>I liked it!</span></div>
+    {error && <p className="py-4 text-red-500">{error}</p>}
+    {isLoading ? (
+    <p>Loading Media...</p>
+    ) : (
+      <div className="mx-16">
+        <div >
+      {
+        media?.map(aMedia => (
+          <>
+          <div 
+          key={aMedia.$id}
+          className="p-4 my-2 rounded-md border-b leading-8">
+             <div className="font-bold">{aMedia.name}</div>
     </div>
     <div className="flex gap-4 mt-4 justify-start">
-    <Link className="bg-green-400 px-4 py-2 rounded-md uppercase text-sm font-bold tracking-widest" href={'/show'}>show</Link>
-      <Link className="bg-slate-200 px-4 py-2 rounded-md uppercase text-sm font-bold tracking-widest" href={'/edit'}>Edit</Link>
-      <button className="bg-red-500 px-4 py-2 text-white rounded-md uppercase text-sm font-bold tracking-widest">
+    <Link className="bg-green-400 px-4 py-2 rounded-md uppercase text-sm font-bold tracking-widest" href={`/show/${aMedia.$id}`}>show</Link>
+      <Link className="bg-slate-200 px-4 py-2 rounded-md uppercase text-sm font-bold tracking-widest" href={`/edit${aMedia.$id}`}>Edit</Link>
+      <button 
+      onClick={() => handleDelete(aMedia.$id)}
+      className="bg-red-500 px-4 py-2 text-white rounded-md uppercase text-sm font-bold tracking-widest">
       Delete
       </button>
+            </div>
+            </>
+        )) 
+      }
+     
     </div>
-    </div>
+    </div>)}
   </>  
   );
 }
