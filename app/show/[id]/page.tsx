fc736672d0402a0d5a2be86db1878ca3b7b17b70
rohansinha01 <rohansinha01@gmail.com>
2024-04-19 13:02:10 -0400

    show route working

--- app/show/[id]/page.tsx
+++ app/show/[id]/page.tsx
@@ -1,18 +1,13 @@
-import { Navbar } from "@/app/components/Navbar"
+async function getPostById(postId: string) {
+    const response = await fetch(`http://localhost:3000/api/media/${postId}`, {
+        method: "GET",
+    });
 
-export default function ShowPage() {
-    return(
-    <>
-    <Navbar />
-    <div>
-        <h2 className="text-2xl font-bold my-8">Show Media Entry</h2>
+    return response.json()
+}
 
-        <form className="flex flex-col gap-3 max-w-400">
-            <input type="text" name="name" placeholder="Name" className="py-1 px-4 border rounded-md" />
-            <input type="number" min={1} max={5} name="characters" placeholder="Characters" className="py-1 px-4 border rounded-md" />
-            <input type="number" min={1} max={5} name="story" placeholder="Story" className="py-1 px-4 border rounded-md" />
-            <input type="number" min={1} max={5} name="vibe" placeholder="Vibe" className="py-1 px-4 border rounded-md" />
-        </form>
-        </div>
-    </>
-)}
+export default async function PostID({ params }: any) {
+    const post = await getPostById(params.id)
+
+    return <main>post {params.id} </main>
+}
