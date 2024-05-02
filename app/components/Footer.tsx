36e04d11f4cba9f1bfb58fd574f6ee0266fcadb0
rohansinha01 <rohansinha01@gmail.com>
2024-05-01 22:22:50 -0400

    create button on home page

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,9 +1,17 @@
-
+import Link from "next/link";
 
 export const Footer = () => {
     return (
         <>
-        <div className="p-12 m-6">
+       
+        <div className="p-12 m-6 gap-2">
+        <Link
+                    className="bg-blue-200 px-4 py-2 rounded-md uppercase text-sm font-bold align-center"
+                    href={`/create`}
+                  >
+                    Add New Entry
+                  </Link>
+        <div><br /></div>
         <h1>Rating Reminder:</h1>
         <h3>3-7: I DID NOT LIKE THAT!</h3>
         <h3>8-12: I LIKED THAT!</h3>
